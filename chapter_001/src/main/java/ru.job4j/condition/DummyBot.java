0be3ba9d5df4b44c98a1f0c709a30764cb62799d
Siskinbird <dimachig@gmail.com>
2020-01-18 20:53:06 +0300

    4.1. Глупый бот.[#221798]

--- chapter_001/src/main/java/ru.job4j/condition/DummyBot.java
+++ chapter_001/src/main/java/ru.job4j/condition/DummyBot.java
@@ -9,9 +9,7 @@ public class DummyBot {
         if ("Пока.".equals(question)) {
             rsl = "До скорой встречи.";
         }
-        else if ("Сколько будет 2 + 2 ".equals(question)){
-            rsl ="Это ставит меня в тупик. Задайте другой вопрос.";
-        }
+
         return rsl;
     }
     }
