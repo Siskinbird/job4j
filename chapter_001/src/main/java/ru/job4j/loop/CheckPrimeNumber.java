dd80daa972fada37db9d295a6d50a777084cd0bf
Siskinbird <dimachig@gmail.com>
2020-01-26 16:32:34 +0300

    5.5. Простое число.[#221870]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,11 +3,13 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int finish) {
         boolean prime = true;
-        for (int num=1;num<=finish;num++) {
-            if (finish % 2 == 0) {
+        for (int index = 2; index <= finish; index++) {
+            if (finish % index == 0) {
                 prime = false;
             }
+            break;
         }
         return prime;
     }
 }
+
