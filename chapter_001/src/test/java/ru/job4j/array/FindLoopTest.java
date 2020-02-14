96050faf76dbf2cc117c82057c76dc4eb3c2b6ae
Siskinbird <dimachig@gmail.com>
2020-02-14 10:45:39 +0300

    6.1. Классический поиск перебором.[#221808]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -15,4 +15,40 @@ public class FindLoopTest {
         int expect = 0;
         assertThat(result, is(expect));
     }
+    @Test
+    public void whenArrayHas14ThenNo() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {11, 23, 17};
+        int value = 1;
+        int result = find.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHas14Then1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {11, 14, 17};
+        int value = 14;
+        int result = find.indexOf(input, value);
+        int expect = 1;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHas17Then2() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {11, 23, 17};
+        int value = 17;
+        int result = find.indexOf(input, value);
+        int expect = 2;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHas42Then4() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {11, 23, 17, 33, 42, 64, 39, 77};
+        int value = 42;
+        int result = find.indexOf(input, value);
+        int expect = 4;
+        assertThat(result, is(expect));
+    }
 }
