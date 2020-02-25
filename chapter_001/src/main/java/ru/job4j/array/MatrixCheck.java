b3abf665829e67a3e50c135971732104e2c5ce9f
Siskinbird <dimachig@gmail.com>
2020-02-25 13:51:39 +0300

    6.7.3. Массив из диагонали матрицы. [#221825]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -8,6 +8,13 @@ package ru.job4j.array;
  **/
 
 public class MatrixCheck {
+    /**
+     * @method Моно строка в матрице
+     * @param board
+     * @param row
+     * @return result
+     **/
+
     public static boolean monoHorizontal(char[][] board, int row) { // создаем метод
         boolean result = true;                                    // ждём результат
         for ( int i = 0 ; i<board.length ; i++) {               //диапазон счетчика цыкла строки
@@ -20,6 +27,14 @@ public class MatrixCheck {
         System.out.println();
         return result;                          // возвращаем результат
     }
+
+    /**
+     * @method Моно столбец в матрице
+     * @param board
+     * @param column
+     * @return result
+     */
+
     public static boolean monoVertical(char[][] board, int column) {  // те же яйца только в профиль!
         boolean result = true;
         for ( int i = 0 ; i<board.length ; i++) {
@@ -33,4 +48,18 @@ public class MatrixCheck {
         return result;
     }
 
-}
+    /**
+     * @method Массив из диагонали матрицы
+     * @param board
+     * @return rsl
+     */
+    public static char[] extractDiagonal(char[][] board) {
+        char[] rsl = new char[board.length];
+        int i, j = 0;
+        for (i = 0; i <board.length; i++) {
+            rsl[j] = board[i][j];
+            j++;
+        }
+            return rsl;
+        }
+    }
