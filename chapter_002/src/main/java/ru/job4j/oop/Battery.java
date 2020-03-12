d18fd8d772a49471b60bc1b427645d25b0cfeee1
Siskinbird <dimachig@gmail.com>
2020-03-12 12:58:13 +0300

    1.7. Состояние объекта.[#221969]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+/**
+ * Battery task.
+ * @author  Chizhov Dmitry dima@chig@gmail.com
+ * @since 12.03.2020
+ */
+
+public class Battery {
+    private int load;
+    /**
+     * Метод определяет заряд батареи
+     * @param charge
+     */
+    public Battery(int charge) {
+        this.load = charge;
+    }
+    /**
+     * Метод описывает перекачку заряда первой батареи в заряд второй
+     * @param another
+     */
+    public void exchange(Battery another) {
+        this.load = this.load - another.load;
+        another.load = another.load + another.load;
+    }
+    /**
+     * Мэйн
+     */
+    public static void main(String[] args) {
+        Battery first = new Battery(100);
+        Battery second = new Battery(42);
+        System.out.println("first : " + first.load + " %" + ". second : " + second.load + " %");
+        first.exchange(second);
+        System.out.println("first : " + first.load + " %" + ". second : " + second.load + " %");
+    }
+}
