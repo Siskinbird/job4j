f16e520bb52b15381b5bedb748dc63d63baa69ad
Siskinbird <dimachig@gmail.com>
2020-03-14 16:11:39 +0300

    4. Расстояние между точками в трехмерном пространстве.[#221961]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -2,39 +2,59 @@ package ru.job4j.condition;
 import static java.lang.Math.sqrt;
 import static java.lang.Math.pow;
 
+/**
+ * Class Класс для вычисления расстояния между двумя точками в двух и трёх мерном пространстве
+ * @author Dmitry Chizhov dimachig@gmail.com
+ * @since 14.03.20
+ */
+
 public class Point {
     /**
      * Это поле объекта. Оно доступно только конкретному объекту.
      */
     private int x;
-    /**
-     * И это поле объекта. Оно доступно только конкретному объекту.
-     */
     private int y;
-
+    private int z;
     /**
      * Конструктор, который принимает два параметра.
      * @param first
      * @param second
      */
-    public Point(int first, int second) {
+   public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
-
     /**
-     * Метод принимающий параметр для поиска расстояния меджу тосъчками
+     * Конструктор, который перегружает метод Point
+     * @param x
+     * @param y
+     * @param z
+     */
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    /**
+     * Метод для поиска расстояния меджу точками в двух мерном пространстве
      * @param that
      * @return
      */
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
-
+    /**
+     * Метод находящий расстояние между точками в трёхмерном пространстве
+     * @param that
+     * @return
+     */
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
     public static void main(String[] args) {
-        Point a = new Point(0, 0);
-        Point b = new Point(0, 2);
-        double dist = a.distance(b);
+        Point a = new Point(2, 0, 33);
+        Point b = new Point(7, 0, 21);
+        double dist = a.distance3d(b);
         System.out.println(dist);
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -6,51 +6,43 @@ import org.junit.Test;
 public class PointTest {
     @Test
     public void distance1() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        Point p1 = new Point(0, 0);
-        Point p2 = new Point(2, 0);
-        double expected = 2.0;
-        double out = p1.distance(p2);
+        Point p1 = new Point(0, 0, 0);
+        Point p2 = new Point(13, 86, 47);
+        double expected = 98.863;
+        double out = p1.distance3d(p2);
         Assert.assertEquals(expected, out, 0.001);
 
     }
     @Test
     public void distance2() {
-        int x1 = 3;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        Point p1 = new Point(3, 0);
-        Point p2 = new Point(2, 0);
-        double expected = 1.0;
-        double out = p1.distance(p2);
+        Point p1 = new Point(3, 0, 77);
+        Point p2 = new Point(2, 0, 69);
+        double expected = 8.062;
+        double out = p1.distance3d(p2);
         Assert.assertEquals(expected, out, 0.001);
     }
     @Test
     public void distance3() {
-        int x1 = 0;
-        int y1 = 11;
-        int x2 = 0;
-        int y2 = 17;
-        Point p1 = new Point(0, 11);
-        Point p2 = new Point(0, 17);
-        double expected = 6.0;
-        double out = p1.distance(p2);
-        Assert.assertEquals(expected, out, 7);
+        Point p1 = new Point(0, 11, 32);
+        Point p2 = new Point(12, 17, 23);
+        double expected = 16.155;
+        double out = p1.distance3d(p2);
+        Assert.assertEquals(expected, out, 0.001);
     }
     @Test
     public void distance() {
-        int x1 = 2;
-        int y1 = 0;
-        int x2 = 7;
-        int y2 = 0;
-        Point p1 = new Point(2, 0);
-        Point p2 = new Point(7, 0);
-        double expected = 5.0;
-        double out = p1.distance(p2);
-        Assert.assertEquals(expected, out, 6);
+        Point p1 = new Point(33, 45, 42);
+        Point p2 = new Point(23, 43, 39);
+        double expected = 10.63;
+        double out = p1.distance3d(p2);
+        Assert.assertEquals(expected, out, 0.001);
+    }
+    @Test
+    public void distance222() {
+        Point p1 = new Point(2, 0, 33);
+        Point p2 = new Point(7, 0, 21);
+        double expected = 13.0;
+        double out = p1.distance3d(p2);
+        Assert.assertEquals(expected, out, 0.001);
     }
 }
