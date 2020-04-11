460e3cde0bd02909ea1faa8d49d43f97854fa1b0
Siskinbird <dimachig@gmail.com>
2020-04-11 15:51:44 +0300

    3. Удаление моделей из массива.[#221984]

--- chapter_002/src/main/java/ru/job4j/oop/pojo/Shop.java
+++ chapter_002/src/main/java/ru/job4j/oop/pojo/Shop.java
@@ -1,69 +1,31 @@
 package ru.job4j.oop.pojo;
 
 public class Shop {
-    public Product[] delete(Product[] products, int i) {
-        int count = 0;
-        for (i = count; i < products.length; i++) {
-            Product product = products[i];
-            if (product != null) {
-                products[i] = null;
+
+    /**
+     * Удаление ячейки из массива
+     * Сдвиг заполненных ячеек на место освобождённых
+     * @param products
+     * @param index
+     * @return возвращает массив без пробелов
+     */
+
+    public Product[] delete(Product[] products, int index) {
+        for (int i = index; i < products.length - 1; i++) {
+            if (products[i] != null) {
                 products[i] = products[i++];
-                count++;
-            }
-            if (product != null) {
-                System.out.println(product.getName());
-            } else {
-                System.out.println("null");
+                products[i] = null;
             }
         }
         return products;
     }
-
-    public static void main(String[] args) {
-        Product products[] = new Product[5];
-        products[0] = new Product("Milk", 10);
-        products[1] = new Product("Bread", 4);
-        products[2] = new Product("Egg", 19);
-
-        for (int i = 0; i < products.length; i++) {
-            Product product = products[i];
-            //проверяем, что объект не равен null. тк у нас массив не заполнен ,целиком.
-            if (product != null) {
-                System.out.println(product.getName());
-            } else {
-                System.out.println("null");
-            }
-        }
-
-        System.out.println();
-        System.out.println("Удаляем значение из ячейки с индексом 1");
-        //удаляем значение из ячейки с индексом 1
-        products[1] = null;
-
-        for (int i = 0; i < products.length; i++) {
-            Product product = products[i];
-            //проверяем, что объект не равен null. тк у нас массив не заполнен целиком.
-            if (product != null) {
-                System.out.println(product.getName());
-            } else {
-                System.out.println("null");
-            }
-        }
-
-        System.out.println();
-        System.out.println("Записываем  в ячейку с индексом 1 значение ячейки с индексом 2 и удаляем значение из ячейки с индексом 2");
-        //записываем в ячейку с индексом 1 значение ячейки с индексом 2.
-        products[1] = products[2];
-        //удаляем значение из ячейки с индексом 2.
-        products[2] = null;
-        for (int i = 0; i < products.length; i++) {
-            Product product = products[i];
-            //проверяем, что объект не равен null. так как  у нас массив не заполнен целиком.
-            if (product != null) {
-                System.out.println(product.getName());
-            } else {
-                System.out.println("null");
-            }
-        }
-    }
 }
+
+    //
+    //Для того, чтобы сместить все элементы влево на одну позицию нужно использовать цикл for + index.
+
+     //   products[index] = products[index + 1];
+
+       // После цикла нужно поставить на последнее место в массиве null, чтобы последний и предпоследний элементы не дублировались
+
+      //  products[products.lenght - 1] = null;
