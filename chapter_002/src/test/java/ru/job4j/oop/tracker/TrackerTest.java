b192ec3730cefc53ed8d63828aa0c8e6f7bda2dc
Siskinbird <dimachig@gmail.com>
2020-04-17 11:01:09 +0300

    5. Реализовать класс Tracker[#221985]

--- chapter_002/src/test/java/ru/job4j/oop/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/oop/tracker/TrackerTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.oop.tracker;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 
@@ -16,27 +15,37 @@ public class TrackerTest {
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
     }
+
     @Test
     public void whenNameFind() {
         Tracker tracker = new Tracker();
-        Item igar = new Item("Igar");
-        tracker.add(igar);
-        Item find = tracker.findByName(igar.getName());
-        assertThat(find.getName(), is("Igar"));
+        Item items = new Item("Igar");
+        tracker.add(items);
+        Item found = new Item("Igar");
+        assertThat(tracker.findByName("Igar"), is("Igar"));
     }
-    //@Test
-    //public void whenNameFind(){
-    //    Tracker tracker = new Tracker();
-    //    Item igar = new Item("Igar'");
-    //    taracker.add(igar);
-    //    String id =
-    //    tracker.findByName("Igar'");
-
-    //    assertThat(Item[], is(item.getName()));
-
-
-
-               // , is(tracker.findByName("Igar'")));
-
+    @Test
+    public void whenReplace() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item("Bug");
+        tracker.add(bug);
+        String id = bug.getId();
+        Item bugWithDesc = new Item("Bug with description");
+        tracker.replace(id, bugWithDesc);
+        assertThat(tracker.findById(id).getName(), is("Bug with description"));
     }
- // }
+}
+
+
+//    Tracker tracker = new Tracker();
+//    Item items = new Item("Igar");
+//    tracker.add(items);
+//    Item[] result  = tracker.findByName(items.getName());
+//    assertThat(result, is("Igar"));
+
+//  Tracker tracker = new Tracker();
+// String[] input = {"Vasya", "I", "wanna", "be", "Petya"};
+// Item[] find = tracker.findByName("Vasya");
+// String[] expected = {"Vasya"};
+// assertThat(find, is(expected));
+
