d88d5c418a0be65bfaefa89d0d1c278566677bae
Siskinbird <dimachig@gmail.com>
2020-04-30 11:47:35 +0300

    Интерфейс Input [#221934]

---
+++ chapter_002/src/main/java/ru/job4j/oop/poly/Bus.java
@@ -0,0 +1,17 @@
+package ru.job4j.oop.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void ride() {
+
+    }
+
+    @Override
+    public void passengers(int passengers) {
+    }
+
+    @Override
+    public double refuel(double fuel, double cash) {
+        return 0;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/oop/poly/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.oop.poly;
+
+public interface Transport {
+     public void ride();
+     public void  passengers(int passengers);
+     double refuel(double fuel, double cash);
+}
