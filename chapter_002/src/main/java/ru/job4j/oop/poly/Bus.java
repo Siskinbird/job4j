f8f4256ce9bbf1f1fa6897f449e0b16cfd9302e1
Siskinbird <dimachig@gmail.com>
2020-05-08 08:59:31 +0300

    3. Интерфейс Input[#221934]

--- chapter_002/src/main/java/ru/job4j/oop/poly/Bus.java
+++ chapter_002/src/main/java/ru/job4j/oop/poly/Bus.java
@@ -3,21 +3,18 @@ package ru.job4j.oop.poly;
 public class Bus implements Transport {
     @Override
     public void ride() {
-        int go = 1;
-        int stop = 0;
+        System.out.println("Едет едет наш автобус");
     }
 
     @Override
     public void passengers(int passengers) {
-        if (passengers > 0 ){
-            Bus bus1 = new Bus();
-            bus1.ride();
-        }
+        System.out.println("42 пассажирёнка");
     }
 
     @Override
     public double refuel(double fuel) {
         double litter = 49.99;
-        return fuel * litter;
+        double cash = fuel * litter;
+        return cash;
     }
 }
