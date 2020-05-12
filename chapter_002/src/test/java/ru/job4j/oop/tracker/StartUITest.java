8f44c8927f7450b2c0d757fa9c4f3c546e0fd265
Siskinbird <dimachig@gmail.com>
2020-05-12 11:18:39 +0300

    Тестирование. Подготовка данных. [#221941]

--- chapter_002/src/test/java/ru/job4j/oop/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/oop/tracker/StartUITest.java
@@ -5,6 +5,9 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class StartUITest {
+    /**
+     * Тест метода на добавление заявки
+     */
     @Test
     public void whenAddItem() {
         String[] answers = {"Fix PC"};
@@ -15,4 +18,32 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
     }
+    /**
+     * Тест метода на перезапись заявки
+     */
+    @Test
+    public void whenItemReplaced() {
+        Tracker tracker = new Tracker();  // Создаём объект tracker
+        Item item = new Item("new item"); // Создаём объект item
+        tracker.add(item); // добавляем item в tracker , item'у присваивается строка с id
+        String[] answers = { item.getId(), "replaced item"}; // Создаём массив строк имитируя результат работы с пользователем
+        StartUI.renameItem(new StubInput(answers), tracker); // Запуск интерфейса, выбор действия, изменение объекта tracker
+        Item replaced = tracker.findById(item.getId()); //  Ищем по item.id замененный item в объекте tracker
+        assertThat(replaced.getName(), is("replaced item"));
+
+    }
+    /**
+     * Тест метода на удаление заявки
+     */
+    @Test
+    public void whenItemDeleted(){
+        Tracker tracker = new Tracker(); // Создаём объект tracker
+        Item item = new Item("new item"); // Создаём объект item
+        tracker.add(item); // добавляем item в tracker , item'у присваивается строка с id
+        String[] answers = {item.getId(), "item deleted"}; // ответ при удачном удалении
+        StartUI.delItem(new StubInput(answers), tracker); // Запуск интерфейса, выбор действия, изменение объекта tracker
+        Item deleted = tracker.findById(item.getName()); // Проверка на отсутствие итема в трекере
+        assertNull(deleted); // Сравнение результата проверки с null
+    }
 }
+
