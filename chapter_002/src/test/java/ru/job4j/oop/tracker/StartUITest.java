c7238483313d26e30955ad9dd175706d38d67445
Siskinbird <dimachig@gmail.com>
2020-05-13 13:54:30 +0300

    Написать тесты на StartUI. [#221944]

--- chapter_002/src/test/java/ru/job4j/oop/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/oop/tracker/StartUITest.java
@@ -1,49 +1,18 @@
 package ru.job4j.oop.tracker;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class StartUITest {
-    /**
-     * Тест метода на добавление заявки
-     */
     @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
-    }
-    /**
-     * Тест метода на перезапись заявки
-     */
-    @Test
-    public void whenItemReplaced() {
-        Tracker tracker = new Tracker();  // Создаём объект tracker
-        Item item = new Item("new item"); // Создаём объект item
-        tracker.add(item); // добавляем item в tracker , item'у присваивается строка с id
-        String[] answers = {item.getId(), "replaced item"}; // Создаём массив строк имитируя результат работы с пользователем
-        StartUI.renameItem(new StubInput(answers), tracker); // Запуск интерфейса, выбор действия, изменение объекта tracker
-        Item replaced = tracker.findById(item.getId()); //  Ищем по item.id замененный item в объекте tracker
-        assertThat(replaced.getName(), is("replaced item"));
-
-    }
-    /**
-     * Тест метода на удаление заявки
-     */
-    @Test
-    public void whenItemDeleted() {
-        Tracker tracker = new Tracker(); // Создаём объект tracker
-        Item item = new Item("new item"); // Создаём объект item
-        tracker.add(item); // добавляем item в tracker , item'у присваивается строка с id
-        String[] answers = {item.getId(), "item deleted"}; // ответ при удачном удалении
-        StartUI.delItem(new StubInput(answers), tracker); // Запуск интерфейса, выбор действия, изменение объекта tracker
-        Item deleted = tracker.findById(item.getName()); // Проверка на отсутствие итема в трекере
-        assertNull(deleted); // Сравнение результата проверки с null
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
     }
 }
-
