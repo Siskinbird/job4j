9ea06a45432d1a76cbb56556d394e3957074eefb
Siskinbird <dimachig@gmail.com>
2020-07-10 17:13:20 +0300

    6. Изменить программу Tracker из 2-го модуля [#221903]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,17 +1,19 @@
 package ru.job4j.tracker;
 
-import java.util.Arrays;
 import java.util.Random;
+import java.util.ArrayList;
 
 public class Tracker {
     /**
      * Массив для хранения заявок.
      */
-    private final Item[] items = new Item[100];
+ //   private final Item[] items = new Item[100];
+    private final ArrayList<Item> items = new ArrayList<>();
+
     /**
      * Указатель ячейки для новой заявки.
      */
-    private int position = 0;
+ //   private int position = 0;
 
     /**
      * Метод добавления заявки в хранилище
@@ -21,7 +23,7 @@ public class Tracker {
      */
     public Item add(Item item) {
         item.setId(generateId());
-        items[position++] = item;
+       // items[position++] = item;
         return item;
     }
 
@@ -41,8 +43,19 @@ public class Tracker {
      *
      * @return Возвращает массив элементов и новый размер.
      */
-    public Item[] findAll() {
-        return Arrays.copyOf(items, position);
+//    public Item[] findAll() {
+//        return Arrays.copyOf(items, position);
+//    }
+
+
+    public ArrayList<Item> findAll() {
+       ArrayList<Item> result = new ArrayList<>();
+        for (Item item : items) {
+            if (item.getName() != null) {
+                result.add(item);
+            }
+        }
+        return result;
     }
 
     /**
@@ -54,8 +67,8 @@ public class Tracker {
 
     private int indexOf(String id) {
         int rsl = -1;
-        for (int index = 0; index < position; index++) {
-            if (items[index].getId().equals(id)) {
+        for (int index = 0; index < items.size(); index++) {
+            if (items.get(index).getId().equals(id)) {
                 rsl = index;
                 break;
             }
@@ -71,7 +84,7 @@ public class Tracker {
      */
     public Item findById(String id) {
         int index = indexOf(id);
-        return index != -1 ? items[index] : null;
+        return index != -1 ? items.get(index) : null;
     }
 
     /**
@@ -81,18 +94,28 @@ public class Tracker {
      * @return возвращает результат поиска или пустой массив.
      */
 
-    public Item[] findByName(String key) {
-        Item[] found = new Item[position];
-        int counter = 0;
-        for (int index = 0; index < position; index++) {
-            Item current = items[index];
-            if (current.getName().equals(key)) {
-                found[counter] = current;
-                counter++;
+    public ArrayList<Item> findByName(String key) {
+        ArrayList<Item> found = new ArrayList<>();
+        for (Item item : items) {
+            if (item.getName().contains(key)) {
+                found.add(item);
             }
-        }
-        return Arrays.copyOf(found, counter);
+        } return found;
     }
+
+
+//    public Item[] findByName(String key) {
+//        Item[] found = new Item[position];
+//        int counter = 0;
+ //       for (int index = 0; index < position; index++) {
+//            Item current = items[index];
+ //           if (current.getName().equals(key)) {
+//                found[counter] = current;
+//                counter++;
+//            }
+//        }
+//        return Arrays.copyOf(found, counter);
+ //   }
 
     /**
      * Замена заявки id
@@ -103,12 +126,12 @@ public class Tracker {
      */
 
 
-    public boolean replace(String id, Item item) { // Параметры
+    public boolean replace(String id, Item item) {
         boolean result = false;
-        int index = indexOf(id);                // ищем индекс по id
-        if (index != -1) {                      // если находим
-            items[index] = item;               // то записываем в ячейку с найденым индексом item
-            item.setId(id);                  // устанавливаем id
+        int index = indexOf(id);
+        if (index != -1) {
+            items.set(index, item);
+            item.setId(id);
             return true;
         }
         return result;
@@ -116,24 +139,32 @@ public class Tracker {
 
     /**
      * Удаление заявки id
-     *
-     * @param id
+     * @param id - индивидуальный номер заявки
      */
-
-    public boolean delete(String id) {
-        boolean result = false;
-        int index = indexOf(id);
-        int start = index + 1;
-        int distPos = index;
-        int size = position - index;
-        if (index != -1) {
-            System.arraycopy(items, start, items, distPos, size);
-            items[position - 1] = null;
-            position--;
+public boolean  delete(String id) {
+   // boolean result = false;
+    //ArrayList<Item> found = new ArrayList<>();
+    for (Item item : items) {
+        if (item.getId().contains(id)) {
+            items.remove(item);
             return true;
         }
-        return result;
-    }
+    }return false;
+}
+   // public boolean delete(String id) {
+    //        boolean result = false;
+    //        int index = indexOf(id);
+    //        int start = index + 1;
+    //        int distPos = index;
+    //        int size = position - index;
+    //        if (index != -1) {
+    //            System.arraycopy(items, start, items, distPos, size);
+    //            items[position - 1] = null;
+    //            position--;
+    //            return true;
+    //        }
+    //        return result;
+    //    }
 }
 
 
