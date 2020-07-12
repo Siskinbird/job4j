ffc26eabb5e0aec23255a5846658f6dc9b618944
Siskinbird <dimachig@gmail.com>
2020-07-12 15:06:02 +0300

    6. Изменить программу Tracker из 2-го модуля [#221903]

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,28 +1,31 @@
 package ru.job4j.tracker;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Пользовательский интерфейс программы Tracker
  */
 public class StartUI {
-
-    public void init(Input input, Tracker tracker, UserAction[] actions) {
+    public void init(Input input, Tracker tracker, List<UserAction> actions) {
         boolean run = true;
         while (run) {
             this.showMenu(actions);
-            int select = input.askInt("Select: ", actions.length);
-            UserAction action = actions[select];
+            int select = input.askInt("Select: ", actions.size());
+            UserAction action = actions.get(select);
             run = action.execute(input, tracker);
         }
     }
 
     /**
      * Масссив действий юзера
-     * @param actions
+     * @param actions - Действие Юзера
      */
-    private void showMenu(UserAction[] actions) {
+    private void showMenu(List<UserAction> actions) {
         System.out.println("Menu.");
-        for (int index = 0; index < actions.length; index++) {
-            System.out.println(index + ". " + actions[index].name());
+        for (int index = 0; index < actions.size(); index++) {
+            System.out.println(index + ". " + actions.get(index).name());
         }
     }
 
@@ -30,7 +33,7 @@ public class StartUI {
         Input input = new ConsoleInput();
         Input validate = new ValidateInput(input);
         Tracker tracker = new Tracker();
-        UserAction[] actions = {
+        List<UserAction> actions = new ArrayList<>(Arrays.asList(
                 new CreateAction(),
                 new FindAllAction(),
                 new DeleteAction(),
@@ -38,7 +41,7 @@ public class StartUI {
                 new FindByNameAction(),
                 new FindByIdAction(),
                 new ExitAction()
-        };
+        ));
         new StartUI().init(validate, tracker, actions);
     }
 }
