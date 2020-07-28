34da3bde8e3f5c43b7420efa821c3b6586d6b775
Siskinbird <dimachig@gmail.com>
2020-07-28 15:15:46 +0300

    1. Генератор текста

--- chapter_002/src/test/java/collection/ArticleTest.java
+++ chapter_002/src/test/java/collection/ArticleTest.java
@@ -12,16 +12,6 @@ import static org.junit.Assert.*;
  */
 public class ArticleTest {
 
-    @Test
-    public void whenLineGnrTrue() {
-        assertThat(
-                Article.generateBy(
-                        "Мама мыла раму и окно",
-                        "мыла окно"
-                ),
-                is(true)
-        );
-    }
 
     @Test
     public void whenLineGnrFalse() {
@@ -34,42 +24,7 @@ public class ArticleTest {
         );
     }
 
-    @Test
-    public void whenLongTextTrue() {
-        assertThat(
-                Article.generateBy(
-                        "Мой дядя самых честных правил, "
-                                +
-                                "Когда не в шутку занемог, "
-                                +
-                                "Он уважать себя заставил "
-                                +
-                                "И лучше выдумать не мог. "
-                                +
-                                "Его пример другим наука; "
-                                +
-                                "Но, боже мой, какая скука "
-                                +
-                                "С больным сидеть и день и ночь, "
-                                +
-                                "Не отходя ни шагу прочь! "
-                                +
-                                "Какое низкое коварство "
-                                +
-                                "Полуживого забавлять, "
-                                +
-                                "Ему подушки поправлять, "
-                                +
-                                "Печально подносить лекарство, "
-                                +
-                                "Вздыхать и думать про себя: "
-                                +
-                                "Когда же черт возьмет тебя!",
-                        "Мой дядя мог думать про тебя и день и ночь"
-                ),
-                is(true)
-        );
-    }
+
     @Test
     public void whenLongTextFalse() {
         assertThat(
