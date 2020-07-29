b4bdde804b59769e76d5f613451d4fcceb523e8f
Siskinbird <dimachig@gmail.com>
2020-07-29 17:33:28 +0300

    1. Анонимные классы. [#221770]

--- chapter_002/src/main/java/ru/job4j/lambda/AttachmentSort.java
+++ chapter_002/src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -15,22 +15,19 @@ public class AttachmentSort {
                 new Attachment("image 2", 34),
                 new Attachment("image 3", 13)
         );
-        Comparator comparator = new Comparator() {
+        Comparator<Attachment> comparator = new Comparator<Attachment>() {
             @Override
-            public int compare(Object o1, Object o2) {
-                Attachment left = (Attachment) o1;
-                Attachment right = (Attachment) o2;
-                return left.getSize() - right.getSize();
+            public int compare(Attachment o1, Attachment o2) {
+                return o1.getSize() - o2.getSize();
             }
         };
         attachments.sort(comparator);
         System.out.println(attachments);
-        Comparator<Attachment> nameCompare = new Comparator() {
+
+        Comparator<Attachment> comparator1 = new Comparator<Attachment>() {
             @Override
-            public int compare(Object o1, Object o2) {
-                Attachment first = (Attachment) o1;
-                Attachment second = (Attachment) o2;
-                return first.getName().compareTo(second.getName());
+            public int compare(Attachment o1, Attachment o2) {
+                return o1.getName().compareTo(o2.getName());
             }
         };
     }
