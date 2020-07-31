5d918b50eb4efbadaf07c991c8534697f28458bd
Siskinbird <dimachig@gmail.com>
2020-07-31 13:24:46 +0300

    3. Ссылки на методы. [#221768]

--- chapter_002/src/main/java/ru/job4j/lambda/RefMethod.java
+++ chapter_002/src/main/java/ru/job4j/lambda/RefMethod.java
@@ -13,11 +13,12 @@ public class RefMethod {
         Consumer<String> out = RefMethod::cutOut;
         names.forEach(out);
     }
-    public static void cutOut(String value){
-        if (value.length() > 10){
+    public static void cutOut(String value) {
+        if (value.length() > 10) {
             System.out.println(value.substring(0, 10) + "..");
         } else {
             System.out.println(value);
         }
     }
 }
+
