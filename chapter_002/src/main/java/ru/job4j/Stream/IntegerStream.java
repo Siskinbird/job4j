ccb37012815c22a5ba880324efab13d9a5b6ea33
Siskinbird <dimachig@gmail.com>
2020-08-07 17:05:51 +0300

    0. Stream API [#221773]

---
+++ chapter_002/src/main/java/ru/job4j/Stream/IntegerStream.java
@@ -0,0 +1,15 @@
+package ru.job4j.Stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Using a stream to filter positive integers
+ */
+public class IntegerStream {
+    public static void main(String[] args) {
+        List<Integer> integers = List.of(7, -3, -34, 78, 43, 43, -34, 65, -55, -19, 9);
+        List<Integer> positive = integers.stream().filter(integer -> integer > 0).collect(Collectors.toList());
+        positive.forEach(System.out::println);
+    }
+}
