860c79e3ddfb2701f06a2091b192091deb316698
Siskinbird <dimachig@gmail.com>
2020-08-13 17:44:55 +0300

    0. Stream API улучшения [#221780]

---
+++ chapter_002/src/main/java/ru/job4j/stream/StudentLevel.java
@@ -0,0 +1,30 @@
+package ru.job4j.stream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * StudentLevel
+ * @author Dmitrii Chizhov (dimachig@gmail.com)
+ * @since 13.08.2020
+ * @version 1.00
+ */
+public class StudentLevel {
+    /**
+     * The method "levelOf" should return a list of students who have a certificate score greater than bound.
+     * @param students - List of students
+     * @param bound - Passing score
+     * @return - list of students who have a certificate score greater than bound
+     */
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .flatMap(Stream::ofNullable)
+                .collect(Collectors.toList())
+                .stream()
+                .sorted(Comparator.comparing(Student::getScore))
+                .takeWhile(st -> st.getScore() > bound )
+                .collect(Collectors.toList());
+    }
+}
