d1e2bb86e355ffc637a6ed745105992305bbf558
Siskinbird <dimachig@gmail.com>
2020-09-16 10:50:31 +0300

    Стажет. Аттестация 16.09.2020

--- chapter_002/src/main/java/ru/job4j/stream/Analyze.java
+++ chapter_002/src/main/java/ru/job4j/stream/Analyze.java
@@ -80,7 +80,7 @@ public class Analyze {
                 .flatMap(pupil -> pupil.getSubjects()
                         .stream())
                 .collect(Collectors.groupingBy(Subject::getName
-                        , LinkedHashMap::new, Collectors.averagingDouble(Subject::getScore)))
+                        , LinkedHashMap::new, Collectors.summingDouble(Subject::getScore)))
                 .entrySet()
                 .stream()
                 .map(stringListEntry -> new Tuple(stringListEntry.getKey()
